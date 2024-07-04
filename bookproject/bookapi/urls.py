4fcf38be283b6786c70c6d7d9d57647870e8fe16
prathebaAT <pratheba63@gmail.com>
2024-07-04 18:59:40 +0530

    added basic authentication

--- bookproject/bookapi/urls.py
+++ bookproject/bookapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet , UserViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- bookproject/bookapi/views.py
+++ bookproject/bookapi/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Book
-from .serializers import BookSerializer
+from .serializers import BookSerializer , UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
